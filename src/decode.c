fe08e18bdbcf6b259758a0cf6bdfebba5c15c6e2
HowlingPython <126528389+HowlingPython@users.noreply.github.com>
2025-08-30 17:21:37 -0300

    Refactor opcode decoding
    Refactored opcode decoding logic in decode.c to use uint32_t instead of Instruction for get_opcode, and updated bitwise operations for correctness.

--- src/decode.c
+++ src/decode.c
@@ -4,32 +4,32 @@ Instruction decode(uint32_t instruction_hex){
     Instruction instruction;
     instruction.hex = instruction_hex;
     
-    if (instruction_hex >> 21  == 0x6A2){
+    if (instruction_hex >> 21  == 0x6A2u){
         instruction.opc = HLT;
         return instruction;
     }
     
-    instruction.opc = get_opcode(instruction); // enum Opcodes 
+    instruction.opc = get_opcode(instruction_hex); // enum Opcodes 
 }
 
-Opcode get_opcode(Instruction instruction) {
+ Opcode get_opcode(uint32_t instruction) {
     // Primero probar por bits [31:24]
-    switch ((instruction.hex >> 24) & 0xFF) {
+    switch ((instruction >> 24) & 0xFF) {
         // ADDS
         case 0xB1: return ADDS_immediate;
         // SUBS o CMP
-        case 0xF1: return instruction.rd == 0x1F ? CMP_immediate : SUBS_immediate;
+        case 0xF1: return (instruction & 0xF) == 0x1F ? CMP_immediate : SUBS_immediate;
         // 
         
         default: break;
     }
 
     // Luego probamos por bits [31:21]
-    switch ((instruction.hex >> 21) & 0xFFF) {
+    switch ((instruction >> 21) & 0xFFF) {
         // ADDS
         case 0x559: return ADDS_extended;
         // SUBS o CMP
-        case 0x759: return instruction.rd == 0x1F ? CMP_extended : SUBS_extended;
+        case 0x759: return (instruction & 0xF) == 0x1F ? CMP_extended : SUBS_extended;
         // ANDS shifted
         case 0x750: return ANDS;
         // EOR shifted
@@ -37,7 +37,7 @@ Opcode get_opcode(Instruction instruction) {
         // OOR shifted
         case 0x550 : return ORR;
         // LSL o LRS
-        case 
+        case : ;
         default: break;
     }
 
--- src/utils.h
+++ src/utils.h
@@ -11,7 +11,4 @@ void mem_write64(uint64_t addr, uint64_t val);
 uint16_t mem_read16(uint64_t addr);
 void mem_write16(uint64_t addr, uint16_t val);
 
-uint8_t mem_read8(uint64_t addr);
-void mem_write8(uint64_t addr, uint8_t val);
-
 #endif
